f765239dcf41a72a0ace53ac80162cc03ac1ba84
yangni <yangni@umich.edu>
2018-01-30 16:32:41 -0500

    init index files

---
+++ indexer/Indexer.cpp
@@ -0,0 +1,5 @@
+#include "Indexer.h"
+
+Indexer::Indexer() {
+
+}
---
+++ indexer/Indexer.h
@@ -0,0 +1,12 @@
+/*
+
+Objective: Pulls small dictionaries from the parser and merges them into the 
+master index.
+
+*/
+
+class Indexer {
+    public:
+        Indexer();
+    private:
+};
