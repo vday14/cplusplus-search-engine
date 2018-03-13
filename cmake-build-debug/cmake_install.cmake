169fd44884961da5dcd15c79fc9e8783d24dbdb7
vcday <vcday@umich.edu>
2018-03-13 19:32:31 -0400

    cmake'

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/benbergkamp/Desktop/398/eecs398-search
+# Install script for directory: /Users/veronicaday/Desktop/EECS398/eecs_398/project/eecs398-search
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -35,5 +35,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/benbergkamp/Desktop/398/eecs398-search/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/veronicaday/Desktop/EECS398/eecs_398/project/eecs398-search/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
