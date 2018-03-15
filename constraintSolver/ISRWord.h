1c16250a85fb56d6d8dcc581549cca084fe2ec49
jsclose <jsclose@umich.edu>
2018-03-15 18:46:10 -0400

    working with nick/ben on determining implementation of isr + constrain solver

--- constraintSolver/ISRWord.h
+++ constraintSolver/ISRWord.h
@@ -13,8 +13,45 @@ class ISRWord : ISR
 	public:
 		unsigned GetDocumentCount( );
 		unsigned GetNumberOfOccurrences( );
-		virtual Post *GetCurrentPost( );
+		ISR* DocumentEnd;
+		// Returns
+		virtual Location First( );
 
+		//Returns next post of a word given current location
+		virtual Location Next( );
+
+		//Calls seek onto one past the current end doc location
+		//Return first instance of word at new document
+		virtual Location NextDocument( );
+		//Returns first instance of word after target location
+		virtual Location Seek( Location target);
+
+
+		virtual ISR *GetDocumentISR( );
+
+		//Returns the location of the end of the document
+		virtual Location GetEndDocument( );
+
+		//Current location of word on disk
+		Location currentLocation;
+		char* term;
+		char* masterIndex;
+		vector<size_t> listOfChunks;
+		size_t currentChunk;
+		char* currentMemMap;
+
+		//set member variables to all of the chunks that occur, update current chunk
+		virtual getChunks();
+
+
+
+	ISRWord(char * term_in ) : term( term_in )
+			{
+				getChunks();
+				currentLocation = First();
+
+
+			}
 	};
 
 
