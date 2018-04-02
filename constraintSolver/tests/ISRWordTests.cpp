00efc351db456522d2d5b39ccf941f5947be2415
Nicholas Yang <parablank@gmail.com>
2018-04-02 17:11:50 -0400

    better test

--- constraintSolver/tests/ISRWordTests.cpp
+++ constraintSolver/tests/ISRWordTests.cpp
@@ -15,7 +15,7 @@ int main ( )
 	{
 	//char* query = "iphone";
 	clock_t start = clock();
-	ISRWord queryWord("trump");
+	ISRWord queryWord("%trump");
 	ISREndDoc endDocs;
 	vector<size_t> locations;
 	vector<DocumentEnding> docEnds;
