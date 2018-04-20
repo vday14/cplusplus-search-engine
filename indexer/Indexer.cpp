e49da831c510a51a4e1b11d4b0e6184147378103
Nicholas Yang <parablank@gmail.com>
2018-04-20 14:23:07 -0400

    removed anchor text stuff + fixed up style a bit

--- indexer/Indexer.cpp
+++ indexer/Indexer.cpp
@@ -1,8 +1,7 @@
 #include "Indexer.h"
 
-Indexer::Indexer( ProducerConsumerQueue < DocIndex * > *doc_index_queue_in,
-						ProducerConsumerQueue < unordered_map < string, DocIndex * > > *anchor_in) :
-		pointerToDictionaries( doc_index_queue_in ), AnchorQueue( anchor_in )
+Indexer::Indexer( ProducerConsumerQueue < DocIndex * > *doc_index_queue_in ) :	
+	pointerToDictionaries( doc_index_queue_in )
 	{
 	currentFile = 0;
 	currentlyIndexed = 0;
@@ -13,18 +12,16 @@ Indexer::Indexer( ProducerConsumerQueue < DocIndex * > *doc_index_queue_in,
 
 void Indexer::run()
 	{
-
-
 	while ( *alive || pointerToDictionaries->Size( ) > 0 )
-
 		{
-		if( pointerToDictionaries->Size( ) > 0)
+		if( pointerToDictionaries->Size( ) > 0 )
 			{
-
 			DocIndex *dictionary = pointerToDictionaries->Pop( );
-			//numberDocsIndexed++;
-			if( (numberDocsIndexed++) % 100 == 0 )
-				cout << "Number of documents indexed :: " << to_string(numberDocsIndexed) << endl;
+			if( ( numberDocsIndexed++ ) % 100 == 0 ) 
+				{
+				std::cout << "Number of documents indexed :: " 
+					<< to_string(numberDocsIndexed) << "\n";
+				}
 
 			if ( numberDocsIndexed == 50000 )
 				break;
@@ -37,25 +34,31 @@ void Indexer::run()
 				{
 				if ( word.first.at( 0 ) == '=' )
 					{
-                    if(word.second.front() == 0)
+                    if( word.second.front( ) == 0) 
                         docEnd.url = word.first.substr(1, word.first.length());
-                    else if(word.second.front() == 1)
-                        docEnd.title = word.first.substr(1, word.first.length());
+                    else if( word.second.front( ) == 1 ) 
+						{
+						docEnd.title = "No title";
+						string title = word.first.substr(1, word.first.length());
+						if(title != "")
+                        	docEnd.title = word.first.substr(1, word.first.length());
+						}
 					continue;
 					}
-				string resized = word.first;
-				if(resized.size() > IndexerConstants::maxWordSize) {
-					resized.resize(IndexerConstants::maxWordSize);
-				}
 
-				chunkDictionary[resized].docFrequency++;
+				string resized = word.first;
+
+				if(resized.size() > IndexerConstants::maxWordSize)
+					resized.resize(IndexerConstants::maxWordSize);
+
+				chunkDictionary[ resized ].docFrequency++;
                 indexedCount += word.second.size( );
 				currentBlockNumberWords += word.second.size( );
 				
-
 				for ( auto location : word.second )
 					{
-					masterDictionary[ resized ].push_back( currentlyIndexed + location );
+					masterDictionary[ resized ].push_back( currentlyIndexed + 
+						location );
 					}
 				}
 
@@ -63,33 +66,24 @@ void Indexer::run()
 			docEnd.docEndPosition = currentlyIndexed;
 			docEnd.docNumWords = indexedCount;
 			docEndings.push_back( docEnd );
-			//add the url to the ->doc end map
-			urlToDocEndings[ docEnd.url ] = docEnd.docEndPosition;
-
 
 			if ( currentBlockNumberWords >= IndexerConstants::chunkSizeLimit )
 				{
-				cout << " --- Saving current chunk --- " << endl;
+				std::cout << " --- Saving current chunk ---\n";
 				save( );
 				saveWordSeek( );
 				reset( );
 				}
 			delete dictionary;
 			}
-
-
 		}
-	cout << "Indexer is shutting down" << endl;
+	std::cout << "Indexer is shutting down\n";
 	save( );
 	saveWordSeek( );
 	reset( );
 	saveChunkDictionary( );
-
-	//	unordered_map < string, DocIndex * > anchorDict = AnchorQueue->Pop( );
-	//SaveAnchorText( &anchorDict );
-	cout << " Indexer has finished running" << endl;
+	std::cout << " Indexer has finished running\n";
 	return;
-
 	}
 
 void Indexer::save()
@@ -97,17 +91,17 @@ void Indexer::save()
 	MMDiskHashTable seeker( util::GetCurrentWorkingDir( ) +
 							IndexerConstants::pathToIndex +
 							to_string( currentFile ) + "-seek.txt",
-							IndexerConstants::chunkSeekKeySize, IndexerConstants::chunkSeekValueSize );
+							IndexerConstants::chunkSeekKeySize, 
+							IndexerConstants::chunkSeekValueSize );
 	string fileName = util::GetCurrentWorkingDir( ) +
                       IndexerConstants::pathToIndex +
                       to_string( currentFile ) + ".txt";
 	int file = open( fileName.c_str( ), O_CREAT | O_WRONLY, S_IRWXU );
 
-	seeker.insert("=numberUniqueWords", to_string(masterDictionary.size()));
-	seeker.insert("=numberWords", to_string(currentBlockNumberWords));
-	seeker.insert("=numberDocs", to_string(currentBlockNumberDocs));
+	seeker.insert("=numberUniqueWords", to_string( masterDictionary.size( ) ) );
+	seeker.insert("=numberWords", to_string( currentBlockNumberWords ) );
+	seeker.insert("=numberDocs", to_string( currentBlockNumberDocs ) );
 
-	// TODO: these should really be c strings
 	string statsHeader = "===STATS==="
                         "\nunique words: " + to_string( masterDictionary.size( )) +
                         "\nnumber words: " + to_string( currentBlockNumberWords ) +
@@ -115,8 +109,7 @@ void Indexer::save()
                         "\n===========\n";
 	write( file, statsHeader.c_str( ), strlen( statsHeader.c_str( )));
 
-	// REALLY GROSS HACK
-	size_t seekOffset = strlen( statsHeader.c_str( ));
+	size_t seekOffset = strlen( statsHeader.c_str( ) );
 	size_t chunkEnd = 0;
 
 	for ( auto word : masterDictionary )
@@ -125,11 +118,11 @@ void Indexer::save()
 			{
 			string resized = word.first;
 			resized.resize( IndexerConstants::maxWordSize );
-			seeker.insert( resized, to_string( seekOffset ));
+			seeker.insert( resized, to_string( seekOffset ) );
 			}
 		else
 			{
-			seeker.insert( word.first, to_string( seekOffset ));
+			seeker.insert( word.first, to_string( seekOffset ) );
 			}
 
 		chunkDictionary[ word.first ].chunks.push_back( currentFile );
@@ -139,7 +132,7 @@ void Indexer::save()
 		int numIndexed = 0;
 		for ( auto location : word.second )
 			{
-			if(chunkEnd < location) {
+			if( chunkEnd < location ) {
 				chunkEnd = location;
 			}
 			chunkDictionary[ word.first ].frequency++;
@@ -177,24 +170,25 @@ void Indexer::save()
 	write( file, docEndingHeader.c_str( ), strlen( docEndingHeader.c_str( )));
 	seekOffset += strlen( docEndingHeader.c_str( ));
 	seeker.insert( "=docEnding", to_string( seekOffset ));
-	int docEndSeekCounter = 0; // save seek every 100 doc ends in the chunk
+	int docEndSeekCounter = 0;
 	for ( auto ending : docEndings )
 		{
 		string title = "";
 		for(char letter : ending.title) {
-			if(letter != ',' && letter != ']' && letter != '[' && letter != '\n' && letter != '\"' && letter != '\\'
-					&& letter != '&' && letter != ':') {
+			if(letter != ',' && letter != ']' && letter != '[' && letter != '\n' 
+				&& letter != '\"' && letter != '\\' && letter != '&' 
+				&& letter != ':') {
 				title.push_back(letter);
 			}
 		}
+		string toDiskTitle = title;
 		string toDiskUrl = ending.url;
 
 		toDiskUrl.erase(std::remove_if(toDiskUrl.begin(),
-												 toDiskUrl.end(),
-													[](unsigned char x){return std::isspace(x);}),
-							 toDiskUrl.end());
+									   toDiskUrl.end(),
+									   [](unsigned char x){return std::isspace(x);}),
+							 		   toDiskUrl.end());
 		toDiskUrl = util::removeAllStr(toDiskUrl, "\\");
-		string toDiskTitle = title;
 		toDiskTitle = util::removeAllStr(toDiskTitle, "\\");
 		toDiskTitle = util::removeAllStr(toDiskTitle, "\\n");
 		toDiskTitle = util::removeAllStr(toDiskTitle, "\\t");
@@ -202,15 +196,10 @@ void Indexer::save()
 		toDiskTitle = util::removeAllStr(toDiskTitle, "\\r");
 		toDiskTitle = util::removeAllStr(toDiskTitle, "\\f");
 
-
-
-
-
-		string docEndString = "[" +
-												toDiskUrl + ", " +
-												toDiskTitle + ", " +
-                                 to_string( ending.docEndPosition ) + ", " +
-                                 to_string( ending.docNumWords ) + "]\n";
+		string docEndString = "[" + toDiskUrl + ", " + 
+								toDiskTitle + ", " +
+                                to_string( ending.docEndPosition ) + ", " +
+                                to_string( ending.docNumWords ) + "]\n";
 		write( file, docEndString.c_str( ), strlen( docEndString.c_str( )));
 		docEndSeekCounter++;
 		if ( docEndSeekCounter == IndexerConstants::saveEveryXEntries )
@@ -222,7 +211,6 @@ void Indexer::save()
 		}
 	chunkEndLocation.push_back(chunkEnd);
 	close( file );
-        //seeker.CloseFile();
 	}
 
 void Indexer::saveChunkDictionary()
@@ -256,7 +244,6 @@ void Indexer::saveChunkDictionary()
 		string key = "=chunk" + to_string(currentChunk);
 		dhtChunk.insert(key, to_string(location));
 		currentChunk++;
-
 		}
 	}
 
@@ -308,56 +295,3 @@ void Indexer::Kill()
 	*(this->alive) = false;
 	//currentFile++;
 	}
-
-
-void Indexer::SaveAnchorText( unordered_map < string, DocIndex * > *anchorDict )
-	{
-
-	//TODO create pointer to anchor
-
-
-	//pointerToAnchor->Pop();
-	//pass a dictionary of
-	//map <url string> - >  vector<anchor word>
-	//for each url in map
-	//look up url string in  url -> docEnding map
-	//for each anchor text in url map
-	// create a  anchor text - > list of doc endings
-	//write to disk
-
-	cout << " -- SAVING ANCHOR TEXT --- " << endl;
-	for ( auto const &ent1 : *anchorDict )
-		{
-		auto const &url = ent1.first;
-		//cout << "url: " << outer_key << endl;
-
-		if ( urlToDocEndings.find( url ) != urlToDocEndings.end( ))
-			{
-			size_t docEndForUrl = urlToDocEndings[ url ];
-			//cout << "Urls doc end : " << docEndForUrl << endl;
-
-			}
-
-
-		DocIndex *inner_map = ent1.second;
-
-		for ( auto const &ent2 : *inner_map )
-			{
-
-
-			auto const &inner_key = ent2.first;
-			auto const &inner_value = ent2.second;
-			//cout << "url: " << outer_key << endl;
-			//cout << "anchor text : " << inner_key << endl;
-			//for(auto offset :inner_value)
-			//	cout << "offset " << offset << endl;
-
-
-			}
-
-		}
-
-
-	}
-
-
